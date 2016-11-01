0fd40ea2651702b8fb512061e6bb0ac4c700a125
Raphael Townshend <raphtown@stanford.edu>
2016-11-01 06:47:12 +0000

    Add sherlock-specific directory creation

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ sudo rm -f $IMG
 sudo singularity create -s 4000 $IMG
 sudo singularity bootstrap $IMG $DEF
 sudo singularity exec -B `pwd`:/mnt -w $IMG sh /mnt/tensorflow.sh 
+sudo singularity exec  -B `pwd`:/mnt -w $IMG sh /mnt/sherlock.sh
---
+++ sherlock.sh
@@ -0,0 +1,5 @@
+# Because sherlock doesn't enable singularity overlay, we must create these
+# before moving container to sherlock.
+mkdir -p /share/PI
+mkdir -p /scratch
+mkdir -p /local-scratch
