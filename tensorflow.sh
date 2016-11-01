5e81b1d94849bffb5655e8c271eea5f3fc24f341
Raphael Townshend <raphtown@stanford.edu>
2016-11-01 03:33:27 +0000

    Update runscript

--- tensorflow.sh
+++ tensorflow.sh
@@ -3,7 +3,7 @@ sh /mnt/NVIDIA-Linux-x86_64-$driver_version.run -x
 mv NVIDIA-Linux-x86_64-$driver_version /usr/local/
 sh /mnt/links.sh $driver_version
 
-sh /mnt/cuda-linux64-rel-8.0.44-21122537.run  --toolkit --silent
+sh /mnt/cuda-linux64-rel-8.0.44-21122537.run  --toolkit --noprompt
 tar xvf /mnt/cudnn-7.0-linux-x64-v4.0-prod.tar -C /usr/local
 
 driver_path=/usr/local/NVIDIA-Linux-x86_64-$driver_version
