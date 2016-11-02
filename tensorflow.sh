2ebc024256e3c35901946c556cb00fea4fd88e94
Raphael Townshend <raphtown@stanford.edu>
2016-11-02 00:42:56 +0000

    Update cudnn version

--- tensorflow.sh
+++ tensorflow.sh
@@ -1,10 +1,12 @@
 driver_version=367.48
+cuda="cuda-linux64-rel-8.0.44-21122537.run"
+cudnn="cudnn-8.0-linux-x64-v5.1.tar"
 sh /mnt/NVIDIA-Linux-x86_64-$driver_version.run -x
 mv NVIDIA-Linux-x86_64-$driver_version /usr/local/
 sh /mnt/links.sh $driver_version
 
-sh /mnt/cuda-linux64-rel-8.0.44-21122537.run  --toolkit --noprompt
-tar xvf /mnt/cudnn-7.0-linux-x64-v4.0-prod.tar -C /usr/local
+sh /mnt/$cuda  --toolkit --noprompt
+tar xvf /mnt/$cudnn -C /usr/local
 
 driver_path=/usr/local/NVIDIA-Linux-x86_64-$driver_version
 echo "LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:$driver_path:$LD_LIBRARY_PATH" >> /environment
