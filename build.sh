aeaf14de00f398de594e157a682ee4b7c0d4dbab
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-12-21 18:34:06 -0800

    Changes to get image built

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 IMG="tf-gpu.img"
 DEF="tf-gpu.def"
+echo "Removing old image file"
 sudo rm -f $IMG
+echo "Creating new image file"
 sudo singularity create -s 4000 $IMG
+echo "Bootstrapping image using docker"
 sudo singularity bootstrap $IMG $DEF
+echo "Executing tensorflow.sh to install NVIDIA/CUDA/CuDNN"
 sudo singularity exec -B `pwd`:/mnt -w $IMG sh /mnt/tensorflow.sh 
+echo "Executing sherlock.sh to prepare for Sherlock environment"
 sudo singularity exec  -B `pwd`:/mnt -w $IMG sh /mnt/sherlock.sh
+echo "Exiting build.sh"
